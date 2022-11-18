ea1beecc2171b0232c063f3ecd272c9b6a59a52d
Carlos A. Toruño P <carlos.toruno@gmail.com>
2022-11-18 08:26:42 -0600

    Settings and RunMe

---
+++ Code/settings.R
@@ -0,0 +1,261 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            LAC Country Reports - Settings
+##
+## Author(s):         Carlos A. Toruño Paniagua   (ctoruno@worldjusticeproject.org)
+##                    A. Santiago Pardo G.        (spardo@worldjusticeproject.org)
+##
+## Dependencies:      World Justice Project
+##
+## Creation date:     November 17th, 2022
+##
+## This version:      November 17th, 2022
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Outline:                                                                                                 ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 1.  Required packages                                                                                    ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Required packages
+library(pacman)
+
+# Notes: ggradar2  and ggsankey need to be installed from Github's developer version. Run the following 
+# lines of code in order to install:
+# remotes::install_github("xl0418/ggradar2@9ebc7b6a34462b356e2893d4c77725b70bed5546")
+# devtools::install_github("davidsjoberg/ggsankey")
+
+p_load(char = c(
+  # Visualizations
+  "showtext", "ggtext", "ggsankey", "ggrepel", "ggplotify", "gridExtra", "ggradar2",
+  
+  # Data Loading
+  "haven", "readxl",
+  
+  # Good 'ol Tidyverse
+  "tidyverse"
+  
+))
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 2.  SharePoint Path                                                                                      ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# SharePoint path
+if (Sys.info()["user"] == "carlostorunopaniagua") {
+  path2SP <- paste0("/Users/carlostorunopaniagua/OneDrive - World Justice Project/Data Analytics/")
+  
+} else if (Sys.info()["user"] == "santiagopardo") {
+  path2SP <- paste0("/Users/santiagopardo/OneDrive - World Justice Project/Data Analytics/")
+  
+} else if (Sys.info()["user"] == "jaeheelee"){
+  path2SP <- paste0("/Users/jaeheelee/Library/CloudStorage/OneDrive-SharedLibraries-WorldJusticeProject/",
+                    "Research - Data Analytics/")
+  
+} else{
+  path2SP <- "PLEASE INSERT YOUR PERSONAL PATH TO THE  WJP - DATA ANALYTICS DIRECTORY"
+}
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 3.  Fonts                                                                                                ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Loading fonts
+path2fonts <- paste0(path2SP, "Country Reports/0. Fonts/")
+font_add(family     = "Lato Full",
+         regular    = paste0(path2fonts, "Lato-Regular.ttf"),
+         italic     = paste0(path2fonts, "Lato-LightItalic.ttf"),
+         bold       = paste0(path2fonts, "Lato-Bold.ttf"),
+         bolditalic = paste0(path2fonts, "Lato-BoldItalic.ttf"))
+font_add(family  = "Lato Light",
+         regular = paste0(path2fonts, "Lato-Light.ttf"))
+font_add(family  = "Lato Black",
+         regular = paste0(path2fonts, "Lato-Black.ttf"))
+font_add(family  = "Lato Black Italic",
+         regular = paste0(path2fonts, "Lato-BlackItalic.ttf"))
+font_add(family = "FontAwesome", 
+         regular = paste0(path2fonts, "fontawesome-webfont.ttf"))
+font_add(family = "FontAwesome5Free-Solid", 
+         regular = paste0(path2fonts, "fa-solid-900.ttf"))
+font_add(family = "FontAwesome 5 Brands", 
+         regular = paste0(path2fonts, "fa-brands-400.ttf"))
+showtext_auto()
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 4.  WJP theme                                                                                            ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Defining a ggplot WJP theme
+WJP_theme <- function() {
+  theme(panel.background   = element_rect(fill = "white",
+                                          size = 2),
+        panel.grid.major   = element_line(size     = 0.5,
+                                          colour   = "#5e5c5a",
+                                          linetype = "dotted"),
+        panel.grid.minor   = element_blank(),
+        axis.title.y       = element_text(family = "Lato Full",
+                                          face     = "plain",
+                                          size     = 10,
+                                          color    = "#524F4C",
+                                          margin   = margin(0, 10, 0, 0)),
+        axis.title.x       = element_text(family = "Lato Full",
+                                          face     = "plain",
+                                          size     = 10,
+                                          color    = "#524F4C",
+                                          margin   = margin(10, 0, 0, 0)),
+        axis.text.y        = element_text(family = "Lato Full",
+                                          face     = "plain",
+                                          size     = 10,
+                                          color    = "#524F4C"),
+        axis.text.x = element_text(family = "Lato Full",
+                                   face   = "plain",
+                                   size   = 10,
+                                   color  = "#524F4C"),
+        axis.ticks  = element_blank(),
+        plot.margin  = unit(c(10, 10, 10, 10), "points")
+  ) 
+}
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 5.  Color Palette                                                                                        ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+mainCOLOR      <- c("#2a2a9A")
+countryPalette <- c("#2a2a94", "#a90099", "#3273ff", "#f0585f", "#b374ff", "#43a9a7", "#efa700", "#2c6d4f")
+binPalette     <- c("#003b8a", "#fa4d57")
+barsPalette    <- c("#2a2a9A", "#E2E2F7")
+glinesPalette  <- c("#2a2a94", "#a90099", "#3273ff")
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 6.  Groups of countries                                                                                  ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Defining country groups
+eastCaribbean.ls <- c(
+  "Antigua and Barbuda", 
+  "Barbados",
+  "Dominica",
+  "Grenada",
+  "St. Kitts and Nevis",
+  "St. Lucia",
+  "St. Vincent and the Grenadines",
+  "Suriname",
+  "Trinidad and Tobago"
+)
+
+westCaribbean_and_guianas.ls <- c(
+  "Bahamas",
+  "Dominican Republic",
+  "Guyana",
+  "Haiti",
+  "Jamaica",
+  "Suriname"
+)
+
+southCone.ls <- c(
+  "Argentina",
+  "Brazil",
+  "Paraguay"
+)
+
+andeanCountries.ls <- c(
+  "Bolivia",
+  "Colombia",
+  "Ecuador",
+  "Peru"
+)
+
+centralAmerica.ls <- c(
+  "Belize",
+  "Costa Rica",
+  "El Salvador",
+  "Guatemala",
+  "Honduras",
+  "Nicaragua",
+  "Panama"
+)
+
+# Which country group is gonna be the comparison group?
+map(
+  # We define a list of lists with all possible groups of countries
+  list(eastCaribbean.ls,
+       westCaribbean_and_guianas.ls,
+       southCone.ls,
+       andeanCountries.ls,
+       centralAmerica.ls),
+  
+  # We define a matching function
+  function(country_group){
+    
+    # IF the mainCountry is within the corresponding list of countries then
+    if (!is.na(match(mainCountry, country_group))) {
+      
+      # We set the corresponding list as the comparison group (minus the mainCountry value)
+      comparison_countries.ls <<- country_group[-match(mainCountry, country_group)]
+      
+    }
+  }
+)
+
+# Updating the country color palette to match the length of the group of countries
+countryPalette <- countryPalette[1:4]
+names(countryPalette) <- c(mainCountry, comparison_countries.ls)
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 7.  Creating directories for Outputs                                                                     ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Creating country folder within the Outputs directory
+dir.create(file.path("Outputs", 
+                     str_replace(mainCountry, " ", "_")), 
+           showWarnings = FALSE)
+
+# Creating folders for each chart output within the country directory
+for (plot in 1:18) {
+  dir.create(file.path("Outputs", 
+                       str_replace(mainCountry, " ", "_"),
+                       paste0("figure_", plot),
+                       fsep = "/"), 
+             showWarnings = FALSE)
+}
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 8.  Creating a saving function                                                                           ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+saveIT.fn <- function(chart, suffix = NULL, w, h) {
+  ggsave(plot   = chart,
+         file   = paste0("Outputs/", 
+                         str_replace(mainCountry, " ", "_"),
+                         "figure_", chart,
+                         "figure_", chart, suffix, ".svg"), 
+         width  = w, 
+         height = h,
+         units  = "in",
+         dpi    = 300,
+         device = "svg")
+} 
+  
+
+  
---
+++ RunMe.R
@@ -0,0 +1,40 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            LAC Country Reports - RunMe File
+##
+## Author(s):         Carlos A. Toruño Paniagua   (ctoruno@worldjusticeproject.org)
+##                    A. Santiago Pardo G.        (spardo@worldjusticeproject.org)
+##
+## Dependencies:      World Justice Project
+##
+## Creation date:     November 17th, 2022
+##
+## This version:      November 17th, 2022
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Outline:                                                                                                 ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 0.  Defining Parameters                                                                                  ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Write down the name of the country
+mainCountry <- "Colombia"
+
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## 1.  Loading Settings                                                                                     ----
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# Required Packages, Fonts, ggplot theme, color palettes, comparison countries and other general routines are
+# loaded from the following script:
+source("Code/settings.R")
+
+# Loading General Functions
